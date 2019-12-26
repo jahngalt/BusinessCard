80ad60329f5d75c82573ff085ee740067264d5e2
Oleg Kudimov <ovkudimov@gmail.com>
2019-12-27 02:20:45 +0300

    Card with swiftUI

--- OlegCard/ContentView.swift
+++ OlegCard/ContentView.swift
@@ -10,7 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+        Color(red:1.00, green:0.75, blue:0.46, opacity: 1.0).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("Oleg")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 250, height: 250)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 1)
+                )
+                
+                Text("Oleg Kudimov")
+            .font(.custom("Pacifico-Regular", size: 40))
+                .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+12312312", imageName: "phone.fill")
+                InfoView(text: "mail@mail.com", imageName: "envelope.fill")
+            }
+        }
     }
 }
 
@@ -19,3 +42,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ OlegCard/InfoView.swift
@@ -0,0 +1,36 @@
+//
+//  InfoView.swift
+//  OlegCard
+//
+//  Created by Oleg Kudimov on 12/26/19.
+//  Copyright © 2019 Oleg Kudimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+               
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+               
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "blablba@mail.com", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
